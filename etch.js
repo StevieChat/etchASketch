e0e92e56a8920a0f7f4b495a813420c99975a5fb
Dylan Redman <dylan.j.redman@gmail.com>
2018-07-26 20:17:31 -0500

    Added reset grid logic

--- etch.js
+++ etch.js
@@ -1,7 +1,9 @@
 var mainDiv = document.querySelector('#mainDiv');
+var resetBtn = document.querySelector('#resetBtn');
 var size = 16;
 
 document.addEventListener('mouseover', changeColor, false);
+resetBtn.addEventListener('click', resetGrid, false);
 
 function drawGrid(size){
 
@@ -23,6 +25,21 @@ function drawGrid(size){
 function changeColor(e){
     if(e.target.classList.contains('gridSquare')){
         e.target.style.backgroundColor = "#D3D3D3";
+    }
+}
+
+function resetGrid(){
+    size = prompt("How many squares would you like on each side of the grid?");
+    clearGrid();
+    drawGrid(size);
+}
+
+function clearGrid(){
+    var cNode = mainDiv.firstChild;
+
+    while(cNode){
+        mainDiv.removeChild(cNode);
+        cNode = mainDiv.firstChild;
     }
 }
 
